caffee7a9ec8d5489032b2d6940a251f1993410e
csubhedar <csubhedar1996@gmail.com>
2016-04-30 20:18:46 +0530

    Added files via upload

---
+++ rooafza_rec.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+r_cascade=cv2.CascadeClassifier('output.xml')
+
+cap=cv2.VideoCapture(0)
+
+while(cap.isOpened):
+    ret,img=cap.read()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ru=r_cascade.detectMultiScale(img,1.2,10)
+    for(x,y,w,h) in ru:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imshow('img',img)
+    k=cv2.waitKey(25) & 0xff
+    if k==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
