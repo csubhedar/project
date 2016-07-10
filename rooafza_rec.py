76da981a91024043675b16a2856aa77ed00010be
csubhedar <csubhedar1996@gmail.com>
2016-07-10 11:27:39 +0530

    Update rooafza_rec.py

--- rooafza_rec.py
+++ rooafza_rec.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
 
-r_cascade=cv2.CascadeClassifier('output.xml')
+r_cascade=cv2.CascadeClassifier('rooafza10.xml')
 
 cap=cv2.VideoCapture(0)
 
 while(cap.isOpened):
     ret,img=cap.read()
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ru=r_cascade.detectMultiScale(img,1.2,10)
+    ru=r_cascade.detectMultiScale(gray,1.2,6)
     for(x,y,w,h) in ru:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     cv2.imshow('img',img)
